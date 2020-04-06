23461580ea25b4123e7c6c685cdec301039274e6
Carlos Maniero <carlosmaniero@gmail.com>
2020-04-06 00:19:13 -0300

    Fixes weird encapsulation

--- src/main/java/io/github/maniero/treeheightofabinarytree/Node.java
+++ src/main/java/io/github/maniero/treeheightofabinarytree/Node.java
@@ -1,9 +1,9 @@
 package io.github.maniero.treeheightofabinarytree;
 
 public class Node {
-    Node left;
-    Node right;
-    int data;
+    private Node left;
+    private Node right;
+    private int data;
 
     Node(int data) {
         this.data = data;
@@ -15,5 +15,28 @@ public class Node {
         this.data = data;
         this.left = left;
         this.right = right;
+    }
+
+    public int height() {
+        if(isLastChild()) {
+            return 0;
+        }
+
+        return 1 + Math.max(
+                heightFor(this.left),
+                heightFor(this.right)
+        );
+    }
+
+    private boolean isLastChild() {
+        return this.left == null && this.right == null;
+    }
+
+    public static int heightFor(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return root.height();
     }
 }
--- src/main/java/io/github/maniero/treeheightofabinarytree/Solution.java
+++ src/main/java/io/github/maniero/treeheightofabinarytree/Solution.java
@@ -1,50 +1,7 @@
 package io.github.maniero.treeheightofabinarytree;
 
-import java.util.Scanner;
-
-class Solution {
-
+public class Solution {
     public static int height(Node root) {
-        if (root == null || (root.left == null && root.right == null)) {
-            return 0;
-        }
-
-        int leftHeight = height(root.left);
-        int rightHeight = height(root.right);
-
-        if (leftHeight > rightHeight) {
-            return leftHeight + 1;
-        }
-
-        return rightHeight + 1;
-    }
-
-    public static Node insert(Node root, int data) {
-        if (root == null) {
-            return new Node(data);
-        } else {
-            Node cur;
-            if (data <= root.data) {
-                cur = insert(root.left, data);
-                root.left = cur;
-            } else {
-                cur = insert(root.right, data);
-                root.right = cur;
-            }
-            return root;
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int t = scan.nextInt();
-        Node root = null;
-        while (t-- > 0) {
-            int data = scan.nextInt();
-            root = insert(root, data);
-        }
-        scan.close();
-        int height = height(root);
-        System.out.println(height);
+        return Node.heightFor(root);
     }
 }
