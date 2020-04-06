2f16476575231412a992193a67ce418f9fc65856
Carlos Maniero <carlosmaniero@gmail.com>
2020-04-05 22:45:43 -0300

    Improve test logging

--- src/test/java/io/github/maniero/helloworld/GreaterTest.java
+++ src/test/java/io/github/maniero/helloworld/GreaterTest.java
@@ -1,10 +1,12 @@
 package io.github.maniero.helloworld;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GreaterTest {
+    @DisplayName("It says hello to the world")
     @Test
     void testGreaterSay() {
         assertEquals("Hello, World!", new Greater().sayHello("World"));
